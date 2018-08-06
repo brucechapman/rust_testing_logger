c9b180115196b93e365704eb40f7a8a432204c2f
brucechapman <bruce.chapman.nz@gmail.com>
2018-08-06 16:38:59 +1200

    Improve docs and capture target

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,22 @@
-//! This module supports testing and asserting that appropriate log messages
+//! This crate supports testing and asserting that appropriate log messages
 //! are generated during tests.
 //!
 //! Logs are captured in a thread_local variable so this module DOES NOT need
 //! the test threads to be set to 1.
-//! #Example
+//!
+//! All log levels are captured, but none are sent to any logging system. The
+//! test developer should call the `validate()` function in order to check
+//! the captured log messages.
+//!
+//! # Examples
 //! ```
 //! // use the log crate macros
 //! #[macro_use]
 //! extern crate log;
 //! use log::Level;
 //! extern crate testing_logger;
-//! # fn main() { test_something(); }
+//!
+//! # fn main() { test_something();  test_target();}
 //! # /* Don't put #[test] in code when running doc tests but DO put it in the documentation
 //! #[test]
 //! # */
@@ -23,6 +29,18 @@
 //!         assert_eq!(captured_logs[0].level, Level::Warn);
 //!     });
 //! }
+//!
+//! # /* Don't put #[test] in code when running doc tests but DO put it in the documentation
+//! #[test]
+//! # */
+//! fn test_target() {
+//!     testing_logger::setup();
+//!     log!(target: "documentation", Level::Warn, "targetted log message");
+//!     testing_logger::validate( | captured_logs| {
+//!         assert_eq!(captured_logs.len(), 1);
+//!         assert_eq!(captured_logs[0].target, "documentation");
+//!     });
+//! }
 //! ```
 
 extern crate log;
@@ -31,11 +49,13 @@ use std::cell::RefCell;
 use std::sync::{Once, ONCE_INIT};
 
 /// A captured call to the logging system.
-    pub struct CapturedLog {
+pub struct CapturedLog {
     /// The formatted log message.
     pub message: String,
     /// The level.
     pub level: Level,
+    /// The target.
+    pub target: String
 }
 
 thread_local!(static LOG_RECORDS: RefCell<Vec<CapturedLog>> = RefCell::new(Vec::with_capacity(3)));
@@ -51,7 +71,8 @@ impl Log for TestingLogger {
         LOG_RECORDS.with( |records| {
             let captured_record = CapturedLog {
                 message: format!("{}",record.args()),
-                level: record.level()
+                level: record.level(),
+                target: record.target().to_string()
             };
             records.borrow_mut().push(captured_record);
         });
@@ -80,7 +101,7 @@ pub fn setup() {
 
 /// Used to validate any captured logs.
 ///
-/// the `asserter` closure can check the number, content and level
+/// the `asserter` closure can check the number, message, target and level
 /// of captured logs. As a side effect, the records are cleared.
 pub fn validate<F>(asserter: F)  where F: Fn(&Vec<CapturedLog>) {
     LOG_RECORDS.with( |records| {
